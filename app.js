4f4e1f82dac4744d2777fc052e2badce672cb2b8
fromHome <bzy.fuzy@gmail.com>
2021-07-27 05:57:34 +0800

    test

--- app.js
+++ app.js
@@ -37,10 +37,6 @@ mongoose.connect(
 );
 
 var app = express();
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -72,7 +68,6 @@ app.post(
   }
 );
 
-app.use("/", indexRouter);
 app.use("/api/user", usersRouter);
 app.use("/api/news", newsRouter);
 
